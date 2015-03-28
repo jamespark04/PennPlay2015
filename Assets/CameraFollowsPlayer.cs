736c69e31e44d949b381df9ead6a14cfc0996cad
jamespark04 <jampa@seas.upenn.edu>
2015-03-28 16:27:08 -0400

    initial commit

---
+++ Assets/CameraFollowsPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowsPlayer : MonoBehaviour {
+
+	public Transform player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 newPosition = player.position + offset;
+		transform.position = Vector3.Lerp (transform.position, newPosition, 5f * Time.deltaTime);
+	}
+}
